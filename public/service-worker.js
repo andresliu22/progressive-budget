6aa173ea48b6ed94ca1bd83f9ec1624fe96000d0
Andres Liu <andres.liu22@gmail.com>
2021-11-18 13:55:04 -0500

    Add files to cache to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,9 @@
 const FILES_TO_CACHE = [
     '/',
-    '/style.css',
+    '/styles.css',
     '/index.html',
+    '/db.js',
+    'manifest.webmanifest',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
   ];
